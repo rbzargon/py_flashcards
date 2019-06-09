f6ee53678f0422d619a57b04c6757957ab50f2b0
Cooper King <cpking2@bu.edu>
2019-06-09 15:00:34 -0400

    Put q/a in labelframes

--- main.py
+++ main.py
@@ -12,33 +12,44 @@ root = Tk()
 root.title("PyFlashcards")
 root_frame = ttk.Frame(root, padding="8 8 8 8")
 
+question_frame = ttk.Labelframe(root_frame, text="Question")
+q = ttk.Label(question_frame, text="Question text")
 
+answer_frame = ttk.LabelFrame(root_frame, text="Answer")
+a1 = ttk.Label(answer_frame, text="Answer 1")
+a2 = ttk.Label(answer_frame, text="Answer 2")
+a3 = ttk.Label(answer_frame, text="Answer 3")
+a4 = ttk.Label(answer_frame, text="Answer 4")
 
+root_frame.grid(row=0, sticky='nsew')
+question_frame.grid(row=0, sticky='nsew')
+q.grid(column=0, row=0, sticky='nsew')
+answer_frame.grid(row=1, sticky='nsew')
+a1.grid(column=0, row=0, sticky='nsew')
+a2.grid(column=1, row=0, sticky='nsew')
+a3.grid(column=2, row=0, sticky='nsew')
+a4.grid(column=3, row=0, sticky='nsew')
 
-q = ttk.Label(root_frame, text="Question")
-
-a1 = ttk.Label(root_frame, text="Answer 1")
-a2 = ttk.Label(root_frame, text="Answer 2")
-a3 = ttk.Label(root_frame, text="Answer 3")
-a4 = ttk.Label(root_frame, text="Answer 4")
-
-root_frame.grid( row=0, sticky='nsew')
-q.grid(column=0, row=1, columnspan=4, sticky='nsew')
-a1.grid(column=0, row=2, sticky='ew')
-a2.grid(column=1, row=2, sticky='ew')
-a3.grid(column=2, row=2, sticky='ew')
-a4.grid(column=3, row=2, sticky='ew')
-
+#column, row configs
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
-for i in range(4):
-    root_frame.columnconfigure(i, weight=1)
+
+root_frame.columnconfigure(0, weight=1)
 root_frame.rowconfigure(0, weight=1)
 root_frame.rowconfigure(1, weight=1)
 
+question_frame.rowconfigure(0, weight=1)
+question_frame.columnconfigure(0, weight=1)
 
+for i in range(4):
+    answer_frame.columnconfigure(i, weight=1)
+answer_frame.rowconfigure(0, weight=1)
 
-for child in root_frame.winfo_children():
-    child.grid_configure(padx=5, pady=5)
+def pad_children(frame: ttk.Frame, padx: int = 5, pady: int = 5) -> None:
+    for child in frame.winfo_children():
+        child.grid_configure(padx=padx, pady=pady)
+
+pad_children(question_frame)
+pad_children(answer_frame)
 
 root.mainloop()
