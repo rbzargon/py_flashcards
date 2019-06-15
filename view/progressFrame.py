5c1eee6e35936d71e1b593c817bf5cfafbd36253
Cooper King <cking10@worcester.edu>
2019-06-15 13:09:28 -0400

    Moves frames to view dir

--- view/progressFrame.py
+++ view/progressFrame.py
@@ -7,7 +7,7 @@ class ProgressFrame(ttk.Frame):
     def __init__(self, parent, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
 
-        self.progressbar = ttk.Progressbar(self, maximum=10)
+        self.progressbar = ttk.Progressbar(self)
         self.progressbar.grid(column=0, row=0, sticky='nsew')
 
         self.rowconfigure(0, weight=1)
