af1c962a30a9c72145aef26345e47001917852bc
Cooper King <cking10@worcester.edu>
2019-06-15 18:55:29 -0400

    Make model.quiz an iterable adapter

--- model/quiz.py
+++ model/quiz.py
@@ -1,29 +1,48 @@
-from typing import Iterable, List, Optional, Set, Tuple
+"""
+Cooper King
+CS521
+Due June 30, 2019
+Term project
+Flashcard application GUI - adapter model class to create question/answer
+groups for quiz questions
+"""
+
 import random
+from typing import Iterable, Iterator, List, Set, Tuple
+
+from .questionGroup import QuestionGroup
 
 
-class Quiz:
-    '''Converts tuples of (question, answer) into an iterable instance of
-    (question, correct_answer, wrong_answers)'''
+class QuizModel:
+    """Converts tuples of (question, answer) into an iterable instance of
+    (question, correct_answer, all_answers)
+    >>> quizzes: Iterable[QuestionGroup] = QuizModel(rows=(('q1', 'a1'), ('q2', 'a2'), ('q3', 'a3'), ('q4', 'a4')))
+    >>> questions, answers, all_answers = zip(*quizzes)
+    >>> sorted(questions)
+    ['q1', 'q2', 'q3', 'q4']
+    >>> sorted(answers)
+    ['a1', 'a2', 'a3', 'a4']
+    >>> all([('a1','a2','a3','a4') == tuple(sorted(all_group)) for all_group in all_answers])
+    True"""
+
     def __init__(self, rows: Iterable[Tuple[str, str]]):
         self.answers: Set[str] = set()
         self.question_to_answer: Set[Tuple[str, str]] = set()
         for row in rows:
-            a, q = row
+            q, a = row
             self.answers.add(a)
             self.question_to_answer.add((q, a))
 
-
-    # returns question, correct_answer, wrong_answers
-    def __iter__(self) -> Iterable[Tuple[str, str, Iterable[str]]]:
-        '''Returns (question, correct_answer, wrong_answers)'''
+    # returns question, correct_answer, all_answers
+    def __iter__(self) -> Iterator[QuestionGroup]:
+        '''Returns (question, correct_answer, all_answers)'''
         for question, correct_answer in self.question_to_answer:
-            wrong_answers = Quiz.get_random_wrong_answers(
+            all_answers = QuizModel.get_random_answers(
                 correct_answer, possible_answers=self.answers, length=4)
-            yield (question, correct_answer, wrong_answers)
+            yield QuestionGroup(question, correct_answer, all_answers)
 
     @staticmethod
-    def get_random_wrong_answers(
+    def get_random_answers(
             correct_answer: str, possible_answers: Iterable[str],
             length: int = 4) -> List[str]:
         '''Given a correct answer and possible answers, returns a list of random
@@ -39,9 +58,16 @@ class Quiz:
                 wrong_answer_choices.append('')
 
         random_wrong_answers = []
-        for _ in range(length-1):
+        for _ in range(length - 1):
             choice = random.choice(wrong_answer_choices)
             wrong_answer_choices.remove(choice)
             random_wrong_answers.append(choice)
 
-        return random_wrong_answers
+        random_answers = [correct_answer, *random_wrong_answers]
+        random.shuffle(random_answers)
+        return random_answers
+
+
+if __name__ == '__main__':
+    from doctest import testmod
+    testmod()
