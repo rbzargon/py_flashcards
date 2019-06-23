102bd465f6e4c01d439ccb5f0459b67967a25462
Cooper King <cking10@worcester.edu>
2019-06-23 11:04:31 -0400

    Rename controller.quiz to controller.main

--- controller/main.py
+++ controller/main.py
@@ -1,15 +1,15 @@
 
 from typing import Iterable, Tuple
 from model.quiz import QuizModel
+from view.main import MainView
 
 
-class Quiz:
+class MainController:
 
     def __init__(self, rows: Iterable[Tuple[str, str]]):
         super().__init__()
         quiz_model = QuizModel(rows=rows)
-        
+        view = MainView()
         for quiz in quiz_model:
-            question, answer, all_answers = quiz
-
-    
+            view.question, view.answers, view.correct_answer = (
+                quiz.question, quiz.all_answers, quiz.answer)
