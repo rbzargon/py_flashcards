a69763d2153fc61b1f9150a7f333c3d89652e58d
Cooper King <cking10@worcester.edu>
2019-06-23 11:05:34 -0400

    Add styling and getters/setters to view.question

--- view/question.py
+++ view/question.py
@@ -7,7 +7,7 @@ Flashcard application - view for a question
 """
 
 import tkinter as tk
-from tkinter.ttk import LabelFrame
+from tkinter.ttk import Label, LabelFrame
 from typing import List
 from utils import pad_children
 
@@ -16,9 +16,20 @@ class QuestionView(LabelFrame):
 
     def __init__(self, parent, *args, **kwargs):
         super().__init__(parent, *args, text='Question', **kwargs)
-        self.question = tk.Button(self, text="Question text")
+        self._question = Label(self, text='Question',
+                               anchor='center',
+                               wraplength=480,
+                               )
 
-        self.question.grid(column=0, row=0, sticky='nsew')
+        self._question.grid(column=0, row=0, sticky='nsew')
         self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
         pad_children(self)
+
+    @property
+    def question(self):
+        return self._question['text']
+
+    @question.setter
+    def question(self, question: str):
+        self._question['text'] = question
