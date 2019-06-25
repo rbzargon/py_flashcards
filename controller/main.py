16720ec4d6a66365c968dbb1b1983ae666a3b7cf
Cooper King <cking10@worcester.edu>
2019-06-25 18:56:27 -0400

    Create quiz handling in controller.main

--- controller/main.py
+++ controller/main.py
@@ -8,8 +8,19 @@ class MainController:
 
     def __init__(self, rows: Iterable[Tuple[str, str]]):
         super().__init__()
+
         quiz_model = QuizModel(rows=rows)
-        view = MainView()
-        for quiz in quiz_model:
-            view.question, view.answers, view.correct_answer = (
-                quiz.question, quiz.all_answers, quiz.answer)
+        self.quizzes = tuple(quiz_model)
+        self.index = 0
+        self.view = MainView(next_handler=self.next_handler,
+                             maximum=len(self.quizzes))
+
+    def next_handler(self):
+        '''Controls moving to the next quiz question'''
+        if (self.index < len(self.quizzes)):
+            self.view.question, self.view.answers, self.view.correct_answer = self.quizzes[
+                self.index]
+            self.index += 1
+            self.view.progress_value = self.index
+        else:
+            exit(0)
