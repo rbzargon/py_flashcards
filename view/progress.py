e26a29ae0ae6cb078190c6afdbcf035065b34bfe
Cooper King <cking10@worcester.edu>
2019-06-29 10:06:49 -0400

    Fix progressbar values

--- view/progress.py
+++ view/progress.py
@@ -15,17 +15,18 @@ class ProgressView(ttk.Frame):
     def __init__(self, parent, maximum, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
         self._value: tk.IntVar = tk.IntVar(0)
-        self.progressbar = ttk.Progressbar(self, variable=self._value)
+        self.progressbar = ttk.Progressbar(
+            self, value=0, maximum=maximum)
         self.progressbar.grid(column=0, row=0, sticky='nsew')
-        
+
         self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
         pad_children(self)
 
     @property
     def value(self) -> int:
-        return self._value.get()
+        return self.progressbar['value']
 
     @value.setter
     def value(self, value: int) -> None:
-        self._value.set(value)
+        self.progressbar['value'] = value
